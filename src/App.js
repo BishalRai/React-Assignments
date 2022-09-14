7fd516282647c6aad1649c8ee241227f05220da4
Bishal Rai <bishal6193@gmail.com>
2022-09-15 02:10:15 +0545

    done changes on HTML to display the heart rate.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useState } from 'react';
 
 function App() {
   //define state variable
-  const [age, setAge] = useState(0)
-  const [heartratelimits, setHeartratelimits] = useState(0)
+  const [age, setAge] = useState()
+  const [heartratelimits, setHeartratelimits] = useState()
 
-  function calculate(){
-    const Upper = (220 - age ) * 0.85
+  //Defined function to do the calculation and return the value to heartratelimits using setHeartratelimits
+  function calculate() {
+    const Upper = (220 - age) * 0.85
     const Lower = (220 - age) * 0.65
     setHeartratelimits(<p>{Lower} - {Upper}</p>)
   }
@@ -20,18 +21,18 @@ function App() {
           <label>Age</label>
           <input type="number"
             placeholder="Enter your age here"
+            value={age}
+            onChange={e => setAge(e.target.value)}
           />
         </div>
         <div>
           <label>Heart Rate Limits</label>
-          <output></output>
+          {/* use the stored value of heartratelimits to display */}
+          <output>{heartratelimits}</output> 
         </div>
-
-        <button type="button"> calculate </button>
-
+        <button type="button" onClick={calculate}> CALCULATE </button>
       </form>
     </div>
   );
 }
-
 export default App;
